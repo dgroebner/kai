ad7a1a397c4098cd8c7d7e13d17b7f08e827df43
Danny Gröbner <danny.groebner@gmail.com>
2026-08-23 16:25:09 +0200

    optic fixes

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-const APP_VERSION = '1.4.12';
+const APP_VERSION = '1.4.13';
 define('APP_URL', rtrim($_ENV['APP_URL'] ?? 'https://kai.agent-smith.de', '/'));
 
 // Verhindert, dass JavaScript auf das Session-Cookie zugreifen kann (Schutz vor XSS)
