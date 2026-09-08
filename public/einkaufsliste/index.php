2e6663811b0bdaf3a4a1a4d69bcc51c48500f21d
Danny Gröbner <danny.groebner@gmail.com>
2026-09-08 22:03:03 +0200

    enhance shopping list

--- public/einkaufsliste/index.php
+++ public/einkaufsliste/index.php
@@ -66,12 +66,8 @@ try {
     <title>kai - Einkaufsliste</title>
     <link rel="stylesheet" href="../css/style.css?v=<?= APP_VERSION ?>">
     <!-- FontAwesome oder eigene Icons knnten hier folgen -->
-    <meta name="market-categories"
-          content="<?= htmlspecialchars(json_encode($categoriesGrouped ?? []), ENT_QUOTES, 'UTF-8') ?>">
+    <meta name="market-categories" content="<?= htmlspecialchars(json_encode($categoriesGrouped ?? []), ENT_QUOTES, 'UTF-8') ?>">
     <meta name="unique-cats" content="<?= htmlspecialchars(json_encode($uniqueCats ?? []), ENT_QUOTES, 'UTF-8') ?>">
-    <script src="../js/http.js" defer></script>
-    <script src="../js/pwa-register.js" defer></script>
-    <script src="../js/einkaufsliste.js?v=<?= APP_VERSION ?>" defer></script>
 </head>
 <?php include __DIR__ . '/../shared/body-tag.php'; ?>
 <div class="container">
