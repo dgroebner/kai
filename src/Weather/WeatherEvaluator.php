1d2eb63fa598404a168e39818cb090e1bdf1ad35
Danny Gröbner <danny.groebner@gmail.com>
2026-09-09 21:45:59 +0200

    enhance shopping list

--- src/Weather/WeatherEvaluator.php
+++ src/Weather/WeatherEvaluator.php
@@ -48,10 +48,10 @@ class WeatherEvaluator
 
         // 1. Regenschirm
         $umbrella = false;
-        $umbrellaText = 'Alles trocken, Schirm kann zuhause bleiben.';
+        $umbrellaText = 'Alles trocken, Regenjacka kann zuhause bleiben.';
         if ($maxPrecipProb > 40 || $maxPrecip > 1.0) {
             $umbrella = true;
-            $umbrellaText = 'Nimm \'nen Schirm mit, sonst wirst du klatschnass!';
+            $umbrellaText = 'Besser ne Regenjacke, sonst wirst du klatschnass!';
         }
 
         // 2. Jacke
@@ -77,7 +77,7 @@ class WeatherEvaluator
         // 4. Pool
         $pool = false;
         $poolText = 'Viel zu kalt für den Pool, bleib lieber im Trockenen.';
-        if ($currentTemp >= 17) {
+        if ($windchill >= 17) {
             $pool = true;
             $poolText = 'Pool-Time! Perfektes Wetter zum Reinspringen.';
         }
@@ -106,7 +106,7 @@ class WeatherEvaluator
             if ($t >= $now && $t <= $now + (24 * 3600)) {
                 $prob = $hourlyPrecipProb[$i] ?? 0;
                 $precip = $hourlyPrecip[$i] ?? 0;
-                
+
                 // Kriterien für Wäsche: >30% Wahrscheinlichkeit oder >0.1mm Regen
                 if ($prob > 30 || $precip > 0.1) {
                     if ($t <= $now + (6 * 3600)) $rain6h = true;
@@ -116,7 +116,6 @@ class WeatherEvaluator
             }
         }
 
-        $laundry = false;
         if ($rain6h) {
             $laundry = false;
             $laundryText = 'Besser nicht: Regen in den nächsten 6h.';
